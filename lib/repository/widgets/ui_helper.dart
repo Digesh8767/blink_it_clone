731bc66229f635cd1a613a4a02218dcf80ce2c4b
Digesh Kapadiya <digesh.belovance@gmail.com>
2025-05-16 05:13:28 +0530

    Splash screen and login screen ui complete

---
+++ lib/repository/widgets/ui_helper.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class UiHelper {
+  static customImage({required String img}) {
+    return Image.asset("assets/images/$img");
+  }
+
+  static customText(
+      {required String text,
+      required Color color,
+      required FontWeight fontWeight,
+      String? fontFamily,
+      required double fontSize}) {
+    return Text(
+      text,
+      style: TextStyle(
+          fontSize: fontSize,
+          fontFamily: fontFamily ?? "regular",
+          fontWeight: fontWeight,
+          color: color),
+    );
+  }
+}
